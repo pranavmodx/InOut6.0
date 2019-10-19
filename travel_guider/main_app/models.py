02889c4d4a3a24bf3911a125a648ce5ac07cf177
Pranav Shridhar <pranavmodx@gmail.com>
2019-10-20 00:36:56 +0530

    created react app

--- travel_guider/main_app/models.py
+++ travel_guider/main_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core import RegexValidator
+from django.core.validators import RegexValidator
 
 lang_choices = (
         ('ENG', 'English'),
@@ -18,16 +18,19 @@ class User(models.Model):
     name = models.CharField(max_length=30)
     email = models.EmailField(null=True)
     phone = models.IntegerField()
-    photo = models.ImageField()
+    photo = models.ImageField(blank=True, null=True)
     description = models.TextField()
     lang_known = models.CharField(max_length=3, choices=lang_choices)
     expense_type = models.CharField(max_length=3, choices=expense_choices)
 
-class Guides(models.Model):
+class Guide(models.Model):
     name = models.CharField(max_length=100, verbose_name='NAME of Guide')
     phone_number_regex = RegexValidator(regex='^((\+91|91|0)[\- ]{0,1})?[456789]\d{9}$', message='Please Enter 10/11 digit mobile number or landline as 0<std code><phone number>', code='invalid_mobile')    
     phone = models.CharField(max_length=10, validators=[phone_number_regex])
+    photo = models.ImageField(blank=True, null=True)
     description = models.TextField()
+    no_of_people_handled = models.IntegerField()
+    location = models.CharField(max_length=500, verbose_name='location')
     rate_per_hour = models.IntegerField()
     native_language = models.CharField(max_length=3, choices = lang_choices)
 
