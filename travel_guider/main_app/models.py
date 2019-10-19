7800feb41ad7a3185adc3db9747ac8a25df936ee
Pranav Shridhar <pranavmodx@gmail.com>
2019-10-19 22:59:31 +0530

    added User model, requirements.txt

--- travel_guider/main_app/models.py
+++ travel_guider/main_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    lang_choices = (
+        ('ENG', 'English'),
+        ('HIN', 'Hindi'),
+        ('MAL', 'Malayalam'),
+        ('TAM', 'Tamil'),
+    )
+
+    expense_choices = (
+        ('ECO', 'Economy'),
+        ('BAS', 'Basic'),
+        ('LUX', 'Luxury'),
+    )
+
+    name = models.CharField(max_length=30)
+    email = models.EmailField(null=True)
+    phone = models.IntegerField()
+    photo = models.ImageField()
+    description = models.TextField()
+    lang_known = models.CharField(max_length=3, choices=lang_choices)
+    expense_type = models.CharField(max_length=3, choices=expense_choices)
+
