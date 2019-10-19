04965ab99a7ef627a824839a8019686e57dd6619
JOS PAUL SHAJAN <jospaulshajan@pop-os.localdomain>
2019-10-19 23:00:17 +0530

    Add models for guides and Reqeusts

--- travel_guider/main_app/models.py
+++ travel_guider/main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Guides(models.Model):
+    name = models.CharField(max_length=100, verbose_name='NAME of Guide')
+    phone_number_regex = RegexValidator(regex='^((\+91|91|0)[\- ]{0,1})?[456789]\d{9}$', message='Please Enter 10/11 digit mobile number or landline as 0<std code><phone number>', code='invalid_mobile')    
+    phone = models.CharField(max_length=10, validators=[phone_number_regex])
+    description = models.TextField()
+    rate_per_hour = models.IntegerField()
+    native_language = models.CharField(max_length=3, choices = lang_choices)
+
+class SearchRequest(models.Model):
+    location = models.CharField(max_length=500, verbose_name='location')
+    duration = models.IntegerField()
+    no_of_people = models.IntegerField
