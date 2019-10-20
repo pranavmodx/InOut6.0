c64483c17737c5e4f7fa3a9ef47866ea19c50df2
Naseem <naseemshahvarikodan@gmail.com>
2019-10-20 10:18:34 +0530

    Introduced Search Results

--- frontend/src/components/layout/Search.js
+++ frontend/src/components/layout/Search.js
@@ -7,10 +7,25 @@ export default class Search extends Component {
         return (
             <div>
                     <div className="search">
-                    <h1>Welcome!</h1>
+                    <h1>Travel Hassel Free</h1>
+                    <p>with <strong>PRO</strong> Guides</p>
                     <img className="searchSVG" alt="." src={searchSVG}></img>
                     <input class="input-field" type="text" placeholder="Where to? eg: Banglore" name="location"></input>
                     <img className="locationSVG" src={locationSVG} alt="." ></img>
+                    <div className="searchResults">
+                        <div className="result">
+                            <div className="CityResult">
+                                <h2>New Delhi</h2>
+                                <p>India, Asia</p>
+                            </div>
+                            <div className="BasicResult">
+                                <ul>
+                                    <li><span>50</span>Guides</li>
+                                    <li><span>7</span>Packages</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                     </div>
             </div>
         )
