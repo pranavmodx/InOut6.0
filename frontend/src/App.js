bfa02239cc9f0952f6bd3d33c90975448b97d34a
Naseem <naseemshahvarikodan@gmail.com>
2019-10-20 12:48:09 +0530

    Added Detailed view for Guide

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import Navbar from './components/layout/Header'
 import './App.css';
 import Search from './components/layout/Search';
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <Search></Search>
     </div>
   );
--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
 
+
+
 export default class Header extends Component {
+   
+
     render() {
         return (
             <div>
-                
+                <div class="navcontainer">                
+                <div class="navbar">
+                <ul>
+                    <li class="logo"> <a href="#"> <strong>ProductName</strong>  </a> </li>
+                    <section class="div_navbar_items">
+                    <li class="navbar_items"> <a href="#home"> Home </a> </li>
+                    <li class="navbar_items"> <a href="#about_me"> About Us </a> </li>
+                    <li class="navbar_items"> <a href="#skills"> Top Trending</a> </li>
+                    <li class="navbar_items"> <a href="#projects"> Explore </a> </li>
+                    <li class="navbar_items"> <a href="#contact"> Contact </a> </li>
+                    </section>
+                    <li class="icon">
+                    <a href="#"> &#9776;</a>
+                    </li>
+                </ul>
+                </div>
+                </div>
             </div>
         )
     }
