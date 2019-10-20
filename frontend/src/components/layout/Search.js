139784f6ee09c789d010fb1f3865c274ca5d0be6
Naseem <naseemshahvarikodan@gmail.com>
2019-10-20 08:13:00 +0530

    Introduced Homepage

--- frontend/src/components/layout/Search.js
+++ frontend/src/components/layout/Search.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
+import searchSVG from '../../assets/search.svg'
+import locationSVG from '../../assets/location.svg'
 
 export default class Search extends Component {
     render() {
         return (
             <div>
-                <img src="frontend/src/assets/search.svg"></img>
-                <input class="input-field" type="text" placeholder="Where to? eg: Banglore" name="location"></input>
-                <img src="../../assets/location.svg"></img>
-                
+                    <div className="search">
+                    <h1>Welcome!</h1>
+                    <img className="searchSVG" alt="." src={searchSVG}></img>
+                    <input class="input-field" type="text" placeholder="Where to? eg: Banglore" name="location"></input>
+                    <img className="locationSVG" src={locationSVG} alt="." ></img>
+                    </div>
             </div>
         )
     }
